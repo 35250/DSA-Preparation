597f3f52bdf5b8b66db3cde547164768b7245b37
Ayan Dey <ayandey2285@gmail.com>
2026-05-01 15:10:18 +0530

    Create number of LIS.cpp

---
+++ Dynamic Programming /number of LIS.cpp
@@ -0,0 +1,6 @@
+Problem: Number of Longest Increasing Subsequence (LC: 673)
+Link: https://leetcode.com/problems/number-of-longest-increasing-subsequence/description/
+Approach: Sequence Extension via DP
+  - State: length[i]= LIS ending at i
+           count[i]= number of LIS ending at i
+  - Recurrence: 
